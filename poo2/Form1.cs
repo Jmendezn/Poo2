9b62a2ecaf10038e06868bda342c2af071facd66
Jesus H. Mendez N <jhmn_@hotmail.com>
2018-07-07 12:54:02 -0700

    2 test

--- poo2/Form1.cs
+++ poo2/Form1.cs
@@ -19,6 +19,8 @@ namespace poo2
 
         class Productos
         {
+
+            // calis
             // Propiedades
             public long producto_codigo = 0;
             public string producto_nombre = "";
