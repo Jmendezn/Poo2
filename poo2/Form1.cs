111023ae2acf10110c9639365e7688c5bfa3dd96
Jesus H. Mendez N <jhmn_@hotmail.com>
2018-07-07 12:46:31 -0700

    primero

---
+++ poo2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace poo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Productos
+        {
+            // Propiedades
+            public long producto_codigo = 0;
+            public string producto_nombre = "";
+            public float producto_precio = 0.00f;
+
+            // Constructor
+            public Productos()
+            {
+            }
+
+        }
+
+        private List<Productos> listaProductos = new List<Productos>();
+
+        private void CargaProductos()
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           /* Productos productos = new Productos();
+            productos.producto_codigo = 12345698764;
+            productos.producto_nombre = "Miller";
+            productos.producto_precio = 36.00f;
+
+            richTextBox1.Text += productos.producto_codigo + " " + productos.producto_nombre + " " + productos.producto_precio;
+            */
+
+        }
+    }
+
+  
+}
---
+++ poo2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("poo2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("poo2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e322bb3c-aa58-4454-a01c-d20c3cf13566")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
