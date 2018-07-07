06f6b7b1270dd3054e30296e82fb73594a94d178
Jesus H. Mendez N <jhmn_@hotmail.com>
2018-07-07 14:58:31 -0700

    07-Junio-2018

--- poo2/Form1.cs
+++ poo2/Form1.cs
@@ -29,28 +29,68 @@ namespace poo2
             // Constructor
             public Productos()
             {
+                MessageBox.Show("me llamaron sin parametros");
+            }
+
+            public Productos(long producto_codigo, string producto_nombre, float producto_precio)
+            {
+                // MessageBox.Show("me llamaron con long, string y float");
+                this.producto_codigo = producto_codigo;
+                this.producto_nombre = producto_nombre;
+               // this.producto_precio = producto_precio;
+            }
+            public Productos(long producto_codigo, string producto_nombre, double producto_precio)
+            {
+                //MessageBox.Show("me llamaron con long, string y double");
+                this.producto_codigo = producto_codigo;
+                this.producto_nombre = producto_nombre;
+                this.producto_precio = (float)producto_precio;
             }
 
         }
 
         private List<Productos> listaProductos = new List<Productos>();
+        Productos[] productos = new Productos[10];
 
         private void CargaProductos()
         {
+            dataGridView1.Columns["Column3"].DefaultCellStyle.Format = "N2" ;
+            //for (int i = 0; i < productos.Length; i++)
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
+                    dataGridView1.Rows.Add(productos[i].producto_codigo, productos[i].producto_nombre, productos[i].producto_precio);
 
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           /* Productos productos = new Productos();
-            productos.producto_codigo = 12345698764;
-            productos.producto_nombre = "Miller";
-            productos.producto_precio = 36.00f;
+            /* Productos productos = new Productos();
+             productos.producto_codigo = 12345698764;
+             productos.producto_nombre = "Miller";
+             productos.producto_precio = 36.00f;
 
-            richTextBox1.Text += productos.producto_codigo + " " + productos.producto_nombre + " " + productos.producto_precio;
-            */
-
+             richTextBox1.Text += productos.producto_codigo + " " + productos.producto_nombre + " " + productos.producto_precio;
+             */
+            // Productos productos = new Productos();
+            // Productos productos = new Productos(1,"kaguamon", 36.00);
+            //  Productos productos = new Productos();
+            
+            productos[0] = new Productos(10, "kaguama 1", 32.05);
+            productos[1] = new Productos(11, "kaguama 2", 66.00);
+            productos[2] = new Productos(12, "kaguama 3", 33.10);
+            CargaProductos();
         }
+        
     }
 
   
